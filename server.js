244de3e8fe0312b5c8cd0772a228d82f2dd79262
lyore15 <regjlyons@gmail.com>
2019-03-10 23:08:18 -0400

    deleted jaws

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var methodOverride = require("method-override");
 var exphbs = require("express-handlebars")
 
 var app = express();
-
-if (process.env.JAWSDB_URL){
-    var connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root', 
-        database: 'burgers_db;'
-    });
-}
 
 var PORT = process.env.PORT || 3000;
 
